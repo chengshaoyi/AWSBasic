72de2007a91488c254fa3c2b608711d0e1186a05
ShaoyiCheng <cheng@numericcal.com>
2017-09-28 22:41:23 -0700

    example script for getting url encoded strings and presigning url

--- Lambda/Presigning/lambda_function.py
+++ Lambda/Presigning/lambda_function.py
@@ -4,6 +4,7 @@ import datetime
 import boto3
 from botocore.client import Config
 
+
 import json
 
 print('Loading function')
@@ -11,7 +12,25 @@ print('Loading function')
 # parsing the event to get the networkId, appId and phoneConfig
 # retrieve the S3 file pointer and return a presigned URL, return a response
 def lambda_handler(event, context):
-    pass
+    phoneConfig = event['queryStringParameters']['phoneConfig']
+    netId = event['queryStringParameters']['netId']
+    appId = event['queryStringParameters']['appId']
+    # from these, find presign a url from S3
+    
+    #allStr = phoneConfig+" "+netId+" "+appId+"\n"
+
+    BUCKET_NAME = 'shaoyi-s3-log'
+    FILE_NAME = "20170928.info";
+    s3_client = boto3.client('s3')
+        
+    url = s3_client.generate_presigned_url('get_object',  Params = {'Bucket': BUCKET_NAME, 'Key': FILE_NAME}, ExpiresIn = 100)
+    allStr = url
+    return { 
+        "statusCode": 200, "body": allStr
+    } 
+
+    
+     
     
     
 
